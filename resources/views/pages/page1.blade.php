fd27855114909645798f6f93d3c4da661ef70dba
makhan <generaltra1@gmail.com>
2021-01-29 19:49:57 +0100

    commit:simplification de la page avec l'eritage et la session

--- resources/views/pages/page1.blade.php
+++ resources/views/pages/page1.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>About us | hello city </title>
-    </head>
-    <body class="antialiased">
+@extends('app')
+      @section('content')
       <p>built with &hearts; by makhan traore</p>
       <p>came back to first page <a href="/"></a></p>
-       <footer>
-          <p>&copy Copyright {{date('Y')}}</p>
-       </footer>
-    </body>
-</html>
+      @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -1,17 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>hello city</title>
-    </head>
-    <body class="antialiased">
-       <h1>welcome to senegal </h1>
+@extends('app')
+   @section('content')
+          <h1>welcome to senegal </h1>
           <p>it's currently {{date('h:i A')}}.</p>
-
-       <footer>
-          <p>&copy Copyright {{date('Y')}} <a href="/about-us">About us</a></p>
-       </footer>
-    </body>
-</html>
+   @endsection
