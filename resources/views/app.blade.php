cabe9cb5092ebff26d154fce20f4ae2bbd7cc150
makhan <generaltra1@gmail.com>
2021-01-30 13:27:09 +0100

    commit: ajout et utilisation de route

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,7 +14,7 @@
        <footer>
           <p>
              &copy Copyright {{date('Y')}} 
-             &middot <a href="/about-us">About us</a>
+             &middot <a href="{{route('about')}}">About us</a>
           </p>
        </footer>
     </body>
