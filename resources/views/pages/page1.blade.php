51d6c23b4996e99afcfbad01f0bf01425fd992f6
makhan <generaltra1@gmail.com>
2021-01-31 00:21:01 +0100

    commit: ajout des images

--- resources/views/pages/page1.blade.php
+++ resources/views/pages/page1.blade.php
@@ -2,6 +2,7 @@
       @section('titre',' About us | '.config('app.name'))
     
       @section('content')
+        <img src="images/images2.jpg" alt="images de mettez vous à laise">
         <p>built with &hearts; by makhan traore</p>
         <p>came back to first page <a href="{{route('home')}}"></a></p>
       @endsection
--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,6 +4,7 @@
    @endsection
          
    @section('content')
+      <img src="images/images1.jpg" alt="images de bienvenue">
           <h1>welcome to senegal </h1>
           <p>it's currently {{date('h:i A')}}.</p>
    @endsection
